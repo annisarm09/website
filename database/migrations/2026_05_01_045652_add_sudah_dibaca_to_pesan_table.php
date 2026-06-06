4805d54c798284e481f759428d0508c9bb801578
annisarm09 <ansrzki09@gmail.com>
2026-06-06 15:24:03 +0700

    fix pesan migration

--- database/migrations/2026_05_01_045652_add_sudah_dibaca_to_pesan_table.php
+++ database/migrations/2026_05_01_045652_add_sudah_dibaca_to_pesan_table.php
@@ -10,12 +10,13 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-       Schema::table('pesan', function (Blueprint $table) {
-        // Menambahkan kolom boolean dengan nilai default false (0)
-        $table->boolean('sudah_dibaca')->default(false)->after('isi_pesan');
+{
+    Schema::table('pesan', function (Blueprint $table) {
+        if (!Schema::hasColumn('pesan', 'sudah_dibaca')) {
+            $table->boolean('sudah_dibaca')->default(false);
+        }
     });
-    }
+}
 
     /**
      * Reverse the migrations.
